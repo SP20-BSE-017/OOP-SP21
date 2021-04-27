b42c596e966a3e73d2d88fa5db9f41705b22d188
SP20-BSE-017 <80266777+SP20-BSE-017@users.noreply.github.com>
2021-04-27 18:46:09 +0000

    Add files via upload

---
+++ Lab9_Assignments/src/L9_Assn3/Runner.java
@@ -0,0 +1,11 @@
+package L9_Assn3;
+
+public class Runner {
+    public static void main(String[] args){
+        PhdStudent ps1 = new PhdStudent();
+        gradStudent gs1 = new gradStudent();
+
+        ps1.takeExam();
+        gs1.takeExam();
+    }
+}
---
+++ Lab9_Assignments/src/L9_Assn3/Student.java
@@ -0,0 +1,5 @@
+package L9_Assn3;
+
+public abstract class Student {
+    abstract void takeExam();
+}
